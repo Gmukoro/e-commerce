f5afd2f5d5a9b698507fdebf0beedcf2c9f975c3
gmukoro <gmukoro3@gmail.com>
2024-04-19 20:40:08 +0100

    Updated the footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,26 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import CheckOut from "./components/CheckOut";
 import Navbar from "./components/Navbar";
 import ShoppingContainer from "./components/ShoppingContainer";
 import ItemDetails from "./components/ItemDetails";
+import Hero from "./components/Hero";
 
 import { useSelector, useDispatch } from "react-redux";
 import { total } from "./components/State/Slice/CartSlice";
 import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
-import Hero from "./components/Hero";
+
 const Layout = () => {
   const { isOpen } = useSelector((state) => state.checkout);
   return (
     <div>
       <Navbar />
-
       <Hero />
-
       {isOpen && <CheckOut />}
       <Outlet />
     </div>
   );
 };
+
 const router = createBrowserRouter([
   {
     path: "/",
@@ -37,16 +37,36 @@ const router = createBrowserRouter([
     ],
   },
 ]);
+
 const App = () => {
   const { cartItems } = useSelector((state) => state.cart);
   const dispatch = useDispatch();
+  const [currentDate, setCurrentDate] = useState("");
+
   useEffect(() => {
     dispatch(total());
+    setCurrentDate(getCurrentDate());
   }, [cartItems]);
+
+  const getCurrentDate = () => {
+    const date = new Date();
+    const options = { year: "numeric", month: "long", day: "numeric" };
+    return date.toLocaleDateString(undefined, options);
+  };
+
   return (
     <div className="font-BeVietnamPro">
       <RouterProvider router={router}></RouterProvider>
+      <Footer />
     </div>
+  );
+};
+
+const Footer = () => {
+  return (
+    <footer className="bg-lime-400  text-black text-center py-4">
+      <p>&copy; {new Date().getFullYear()} Gona Pearls. All rights reserved.</p>
+    </footer>
   );
 };
 
